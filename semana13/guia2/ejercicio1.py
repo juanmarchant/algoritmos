2f114db5e7b1dda9aacf431a5d1b661579a159c6
juanmarchant <juanmarchantdev@gmail.com>
2023-06-06 15:52:11 -0400

    Folder guia2 added

---
+++ semana13/guia2/ejercicio1.py
@@ -0,0 +1,14 @@
+# La empresa "Dulce chupetin" necesita un programa en python que maneje el ingreso de datos de sus n trabajadores
+# Para esto se deben utilizar arreglos. Los datos que se piden ingresar son:
+# Nombre trabajador, rut trabajador, horastrabajadas, valor hora, horas extras.
+# Se pide llenar los arreglos, una vez llenos mostrar:
+
+# a)Nombre, rut y sueldo de cada trabajador (el sueldo se calcula: horas trabajadas* valor hora+ (horas extra* valorhora extra)
+# el valor hora extra es el 50% mas del valor hora normal
+# b)Nombre y rut del trabajador con mayor sueldo
+# c)Total de dinero que paga la empresa a sus trabajadores
+# d)Nombre y rut de quien gana el mayor sueldo
+
+
+
+
